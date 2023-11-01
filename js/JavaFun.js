9a188a7f660f7436b60f632ed493e6f22fa85ee5
edferoca <efrojasc@unal.edu.co>
2023-11-01 19:31:45 +0100

    first styles and js

---
+++ js/JavaFun.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("menu-button").addEventListener("mouseenter", function () {
+        document.getElementById("menu").style.display = "block";
+      });
+      /*document.getElementById("menu-button").addEventListener("mouseleave", function () {
+        document.getElementById("menu").style.display = "none";
+      });*/
+
+  });
+  
+  
